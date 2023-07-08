ae359f15749247878ff597775219a4242c1b3314
Sean Henderson <seanhenderson1314@gmail.com>
2023-07-08 11:11:16 +0200

    num_len function

---
+++ num_len.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	num_len(int	num)
+{	
+	int	count;
+	
+	count = 0;
+	if (num == 0)
+		return (1);
+	while (num != 0)
+	{
+		count++;
+		num /= 10;
+	}
+	return (count);
+}
+
+int	main()
+{
+	int	num = 453488882;
+
+	printf("%d\n", num_len(num));
+	return (0);
+}
